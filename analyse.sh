839c8da83c2df3cd4b309c4a991ffe38be390feb
theJovian <pabloat98@outlook.es>
2022-04-09 12:15:14 +0200

    Added full script and sonar cube config for wordpress

---
+++ analyse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "analysing android...";
+cd sample-webview;
+./gradlew sonarqube -Dsonar.host.url=http://localhost:9000;
+
+echo "analysing wordpress...";
+cd ..;
+cd web;
+cd wordpress;
+sonar-scanner -Dsonar.login=fac0a663b8d5003791ffc3caf02e6888302e52a2;
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "hola mundo";
+echo "building android...";
 cd sample-webview;
 ./gradlew assembleDebug;
+
+echo "building wordpress...";
 cd ..;
 cd web;
 php create-phar.php;
