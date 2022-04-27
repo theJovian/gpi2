041a6a61ac181b784fada45c9701061a8457c777
theJovian <pabloat98@outlook.es>
2022-04-27 16:11:09 +0200

    fixed previus change

--- analyse.sh
+++ analyse.sh
@@ -4,7 +4,7 @@
 ##./gradlew sonarqube -Dsonar.host.url=http://localhost:9000;
 
 echo "analysing wordpress...";
-cd ..;
+##cd ..;
 cd web;
 cd wordpress;
 sonar-scanner -Dsonar.login=fac0a663b8d5003791ffc3caf02e6888302e52a2;
