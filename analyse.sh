d81877070767aec77399680ad9fdd7c436c079ca
theJovian <pabloat98@outlook.es>
2022-05-07 18:17:21 +0200

    change analyse

--- analyse.sh
+++ analyse.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-##echo "analysing android...";
-##cd sample-webview;
-##./gradlew sonarqube -Dsonar.host.url=http://localhost:9000;
+echo "analysing android...";
+cd sample-webview;
+./gradlew sonarqube -Dsonar.host.url=http://localhost:9000;
 
 echo "\n\n------------------------------------\n\n"
 
 echo "Analysing wordpress...";
 ##cd ..;
-cd web;
-cd wordpress;
-/opt/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner -Dsonar.login=fac0a663b8d5003791ffc3caf02e6888302e52a2;
+##cd web;
+##cd wordpress;
+##/opt/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner -Dsonar.login=fac0a663b8d5003791ffc3caf02e6888302e52a2;
